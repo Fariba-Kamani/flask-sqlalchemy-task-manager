20463dea31531977a2c46838de38a3cd915224b5
Fariba-Kamani <fariba.kamani87@gmail.com>
2025-02-10 00:13:26 +0100

    Add task

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -2,4 +2,19 @@ document.addEventListener('DOMContentLoaded', function() {
     // Sidenav initialization
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
+
+    // Datepicker initialization
+    
+    let datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, {
+      format: "dd mmm, yyyy",
+      i18n: {done: "Select"}
+
+
+    });
+
+    // select initialization
+    let selects = document.querySelectorAll('select');
+    M.FormSelect.init(selects);
+    
   });
